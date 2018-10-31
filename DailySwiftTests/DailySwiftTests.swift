c0bd8ff93af732a600cf5a9ec33bbb92ffb53946
koyoarai-ist <koyo.arai@i-studio.co.jp>
2018-10-31 19:03:08 +0900

    init project

---
+++ DailySwiftTests/DailySwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  DailySwiftTests.swift
+//  DailySwiftTests
+//
+//  Created by 新井 恒陽 on 2018/10/31.
+//  Copyright © 2018 新井 恒陽. All rights reserved.
+//
+
+import XCTest
+@testable import DailySwift
+
+class DailySwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
